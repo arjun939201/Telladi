1cf83467468008dca752baa305ee4ee77bc48611
arjun939201 <throwuse829@gmail.com>
2025-07-23 23:34:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,137 @@ const dictionary = [
       telugu: "అందం",
       iast: "andam"
     }
+  },
+  {
+  root: "bāgu",
+  label: "n.",
+  meaning: "health",
+  note: "[well-being]",
+  scripts: {
+    telugu: "బాగు",
+    iast: "bāgu"
   }
+},
+{
+  root: "tiṇḍi",
+  label: "n.",
+  meaning: "food",
+  note: "[eatable]",
+  scripts: {
+    telugu: "తిండి",
+    iast: "tiṇḍi"
+  }
+},
+{
+  root: "nīḷḷu",
+  label: "n.",
+  meaning: "water",
+  note: "[liquid]",
+  scripts: {
+    telugu: "నీళ్ళు",
+    iast: "nīḷḷu"
+  }
+},
+{
+  root: "kūrmi",
+  label: "n.",
+  meaning: "love",
+  note: "[affection]",
+  scripts: {
+    telugu: "కూర్మి",
+    iast: "kūrmi"
+  }
+},
+{
+  root: "nesaru",
+  label: "n.",
+  meaning: "sun",
+  note: "[daylight giver]",
+  scripts: {
+    telugu: "నెసరు",
+    iast: "nesaru"
+  }
+},
+{
+  root: "nela",
+  label: "n.",
+  meaning: "moon",
+  note: "[night light]",
+  scripts: {
+    telugu: "నెల",
+    iast: "nela"
+  }
+},
+{
+  root: "nēla",
+  label: "n.",
+  meaning: "earth",
+  note: "[ground]",
+  scripts: {
+    telugu: "నేల",
+    iast: "nēla"
+  }
+},
+{
+  root: "ningi",
+  label: "n.",
+  meaning: "sky",
+  note: "[atmosphere]",
+  scripts: {
+    telugu: "నింగి",
+    iast: "ningi"
+  }
+},
+{
+  root: "konda",
+  label: "n.",
+  meaning: "mountain",
+  note: "[elevation]",
+  scripts: {
+    telugu: "కొండ",
+    iast: "koṇḍa"
+  }
+},
+{
+  root: "eru",
+  label: "n.",
+  meaning: "river",
+  note: "[stream]",
+  scripts: {
+    telugu: "ఏరు",
+    iast: "ēru"
+  }
+},
+{
+  root: "aḍavi",
+  label: "n.",
+  meaning: "forest",
+  note: "[woods]",
+  scripts: {
+    telugu: "అడవి",
+    iast: "aḍavi"
+  }
+},
+{
+  root: "lekka",
+  label: "n.",
+  meaning: "math",
+  note: "[counting]",
+  scripts: {
+    telugu: "లెక్క",
+    iast: "lekka"
+  }
+},
+{
+  root: "chekundaṁ",
+  label: "n.",
+  meaning: "art",
+  note: "[creative work]",
+  scripts: {
+    telugu: "చెకుందం",
+    iast: "cekundaṁ"
+  }
+    }
 ];
 
 function highlightMatch(text, query) {
